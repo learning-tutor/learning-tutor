f0e0119d06da2ef1e0915a9d1f383e2fd2eb6e8c
Lucas Pazzim <lpazzim@gmail.com>
2024-01-08 15:27:59 -0300

    feat(linaria): setup linaria

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { lightTheme } from "./theme";
 import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className={lightTheme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
